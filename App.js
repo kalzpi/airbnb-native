2709b5a9be9f8f878024d0ed114ea8311d689d53
kalzpi <kalzpi@gmail.com>
2020-04-26 19:05:19 -0500

    Project setup done

--- App.js
+++ App.js
@@ -4,6 +4,10 @@ import { Text, Image } from "react-native";
 import { Asset } from "expo-asset";
 import { Ionicons } from "@expo/vector-icons";
 import * as Font from "expo-font";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import Gate from "./components/Gate";
+import store, { persistor } from "./redux/store";
 
 const cacheImages = (images) =>
   images.map((image) => {
@@ -30,7 +34,11 @@ export default function App() {
     return Promise.all(...fontPromises, ...imagePromises);
   };
   return isReady ? (
-    <Text>I'm ready.</Text>
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <Gate />
+      </PersistGate>
+    </Provider>
   ) : (
     <AppLoading
       onError={console.error}
